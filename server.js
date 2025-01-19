68deb8d06c134dbbc726881899710bc5e5ac7944
somdurgeshgupta <somdurgeshgupta@gmail.com>
2025-01-19 15:24:42 +0530

    updated code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv/config');
-const fs = require('fs');
-const https = require('https');
+// const fs = require('fs');
+// const https = require('https');
 const api = process.env.API_URL;
 const path = require('path');
 const express = require('express');
@@ -12,11 +12,11 @@ const bodyParser = require('body-parser');
 const connectToDatabaseMongoose = require('./config/mongoose.js');
 
 // SSL certificate paths (Update with your actual certificate and key paths)
-const privateKey = fs.readFileSync('/home/ubuntu/ssl/private-key.pem', 'utf8');
-const certificate = fs.readFileSync('/home/ubuntu/ssl/certificate.pem', 'utf8');
+// const privateKey = fs.readFileSync('/home/ubuntu/ssl/private-key.pem', 'utf8');
+// const certificate = fs.readFileSync('/home/ubuntu/ssl/certificate.pem', 'utf8');
 // const ca = fs.readFileSync('/path/to/your/ca_bundle.crt', 'utf8');
 
-const credentials = { key: privateKey, cert: certificate};
+// const credentials = { key: privateKey, cert: certificate};
 
 connectToDatabaseMongoose();
 
@@ -48,14 +48,14 @@ const googlelogin = require('./helpers/google-login.js');
 app.use(`${api}/users`, authJwt(), usersRoutes);
 app.use(`${api}/profile`, authJwt(), profileRoutes);
 app.use(`${api}`, googlelogin);
-app.use(`/`, (req, res) => {
+app.use(`/testing`, (req, res) => {
   res.send('Welcome to API');
 });
 
 // Start HTTPS server
 const PORT = process.env.PORT || 4100; // HTTPS default port
-const httpsServer = https.createServer(credentials, app);
+// const httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`HTTPS Server running at https://localhost:${PORT}`);
 });
